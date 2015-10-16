225e2610d6e1da9f9a63c05a5ac1e379b08abaa9
bardec <code@getbraintree.com>
2015-10-16 15:31:43 -0500

    make foos-rfid folder

--- foos-rfid/sender.py
+++ foos-rfid/sender.py
@@ -1,18 +1,26 @@
 import serial
 
 def setup():
-    global device
-    device = serial.Serial(
+    global device_a
+    global device_b
+    device_a = serial.Serial(
                 port='/dev/tty.usbserial-AD026AOK',
                 baudrate='9600',
             )
-    if not device.isOpen():
-        device.open()
+
+    device_b = serial.Serial(
+                port='/dev/tty.usbserial-AD026AOK',
+                baudrate='9600',
+            )
+    if not device_a.isOpen():
+        device_a.open()
+    if not device_b.isOpen():
+        device_b.open()
     loop()
 
 def loop():
-    uuids_for_game = []
-    while len(uuids_for_game) < 4:
+    uuids_for_game = { 'team_a': [], 'team_b': [] }
+    while len() < 4:
         uuid = device.readline().rstrip()
 
         if uuid in uuids_for_game:
@@ -20,6 +28,9 @@ def loop():
         else:
             uuids_for_game.append(uuid)
     send_uuids(uuids_for_game)
+
+def is_game_full(uuids_for_game):
+    pass
 
 def send_uuids(uuids_for_game):
     pass
